35c4ca6d721647eb44024448428a99660b42e385
sew.kim501 <sew.kim501@gmail.com>
2020-06-08 00:41:22 -0700

    update die and core size for jpeg, ibex, bp_single and bp_dual

--- flow/designs/gf14/bp_dual/config.mk
+++ flow/designs/gf14/bp_dual/config.mk
@@ -16,7 +16,13 @@ export ADDITIONAL_LEFS = ./platforms/gf14/bp/lef/gf14_1r1w_d32_w64_m1.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d512_w64_m2_byte.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d64_w124_m2_bit.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d64_w62_m2_bit.lef \
-                         ./platforms/gf14/bp/lef/IN12LP_GPIO18_13M9S30P.lef
+                         ./platforms/gf14/bp/lef/IN12LP_GPIO18_13M9S30P.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1r1w_d32_w64_m1_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d128_w116_m2_bit_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d256_w48_m2_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d512_w64_m2_byte_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w124_m2_bit_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w62_m2_bit_mod.lef
 
 export ADDITIONAL_LIBS = ./platforms/gf14/bp/lib/gf14_1r1w_d32_w64_m1_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d128_w116_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
@@ -24,9 +30,23 @@ export ADDITIONAL_LIBS = ./platforms/gf14/bp/lib/gf14_1r1w_d32_w64_m1_tt_nominal
                          ./platforms/gf14/bp/lib/gf14_1rw_d512_w64_m2_byte_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d64_w124_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d64_w62_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
-                         ./platforms/gf14/bp/lib/IN12LP_GPIO18_13M9S30P_TT_0P8_1P8_25.lib
+                         ./platforms/gf14/bp/lib/IN12LP_GPIO18_13M9S30P_TT_0P8_1P8_25.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1r1w_d32_w64_m1_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d128_w116_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d256_w48_m2_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d512_w64_m2_byte_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w124_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w62_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib
 
-export ADDITIONAL_GDS  = ./platforms/gf14/bp/gds/gf14_1r1w_d32_w64_m1.gds2 \
+export ADDITIONAL_GDS  = ./platforms/gf14/bp/wrappers/gf14_1r1w_d32_w64_m1_mod.gds2 \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d128_w116_m2_bit_mod.gds2 \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d256_w48_m2_mod.gds2 \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d512_w64_m2_byte_mod.gds2 \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w124_m2_bit_mod.gds2 \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w62_m2_bit_mod.gds2 \
+                         ./platforms/gf14/bp/gds/IN12LP_GPIO18_13M9S30P.gds
+
+#export ADDITIONAL_GDS  = ./platforms/gf14/bp/gds/gf14_1r1w_d32_w64_m1.gds2 \
                          ./platforms/gf14/bp/gds/gf14_1rw_d128_w116_m2_bit.gds2 \
                          ./platforms/gf14/bp/gds/gf14_1rw_d256_w48_m2.gds2 \
                          ./platforms/gf14/bp/gds/gf14_1rw_d512_w64_m2_byte.gds2 \
@@ -45,3 +65,9 @@ export SIG_MAP_FILE = ./platforms/gf14/bp/soc_bsg_black_parrot.sigmap
 export ABC_CLOCK_PERIOD_IN_PS = 1250
 
 export PLACE_DENSITY = 0.20
+
+export MACRO_WRAPPERS = ./platforms/gf14/bp/wrappers/wrappers.tcl
+export PDN_CFG ?= ./platforms/gf14/pdn_grid_strategy_13m_9T.top.cfg
+
+# TODO: replace this with max(CHANNEL_WIDTH_[HV]) from IP_global.cfg
+export MACRO_BLOCKAGE_HALO = 25
